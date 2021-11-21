af987e5a22c8f2fd58b895d97e7779a023f9b80c
joseph townshend <joetownshend@mac.com>
2021-11-21 16:19:14 +0000

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,43 @@
+let poem =
+  "Two roads diverged in a yellow wood And sorry I could not travel both And be one traveler long I stood And looked down one as far as I could To where it bent in the undergrowth";
+
+let api = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+let promiseArray = [];
+let split = poem.split(" ");
+
+function setup() {
+  createCanvas(400, 400);
+  background(250, 250, 240);
+  textSize(18);
+  textWrap(WORD);
+  textAlign(CENTER);
+  noLoop();
+}
+
+function preload() {
+  for (const word of split) {
+    getData(word);
+  }
+}
+
+function getData(word) {
+  promiseArray.push(
+    fetch(api + word)
+      .then((r) => r.json())
+      .then((data) => data[0].meanings[0].definitions[0].synonyms[0])
+      .then((res) => {
+        if (res) {
+          return res;
+        } else {
+          return word;
+        }
+      })
+  );
+}
+
+function draw() {
+  Promise.all(promiseArray)
+    .then((values) => values.join(" ").toLowerCase())
+    .then((values) => text(values, height / 2 - 100, width / 2 - 150, 200, 400))
+    .catch((err) => console.log(err));
+}
